db757bbc77b33c3a33c6680b1027e4293f4da7f3
ajbraus <ajbraus@gmail.com>
2013-04-04 17:12:35 -0500

    blah

--- db/migrate/20130404220134_adding_more_to_subscription.rb
+++ db/migrate/20130404220134_adding_more_to_subscription.rb
@@ -1,8 +1,8 @@
 class AddingMoreToSubscription < ActiveRecord::Migration
   def change
-    add_column, :email_subscriptions, :name, :string
-    add_column, :email_subscriptions, :message, :string
-    add_column, :email_subscriptions, :phone, :string
-    add_column, :email_subscriptions, :newsletter, :boolean
+    add_column :email_subscriptions, :name, :string
+    add_column :email_subscriptions, :message, :string
+    add_column :email_subscriptions, :phone, :string
+    add_column :email_subscriptions, :newsletter, :boolean
   end
 end
