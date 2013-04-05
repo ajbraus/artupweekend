efef0a585a8b358cb712d84cf2f5cc330ad13553
ajbraus <ajbraus@gmail.com>
2013-04-05 12:38:56 -0500

    got things organized

--- db/migrate/20130404220134_adding_more_to_subscription.rb
+++ db/migrate/20130404220134_adding_more_to_subscription.rb
@@ -1,6 +1,5 @@
 class AddingMoreToSubscription < ActiveRecord::Migration
   def change
-    add_column :email_subscriptions, :name, :string
     add_column :email_subscriptions, :message, :string
     add_column :email_subscriptions, :phone, :string
     add_column :email_subscriptions, :newsletter, :boolean
