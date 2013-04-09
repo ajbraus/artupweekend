d6691eaa5e71c77b8d8a5b17e371dc62a40d8825
ajbraus <ajbraus@gmail.com>
2013-04-09 12:15:29 -0500

    remove pics

--- db/migrate/20130404220134_adding_more_to_subscription.rb
+++
@@ -1,7 +0,0 @@
-class AddingMoreToSubscription < ActiveRecord::Migration
-  def change
-    add_column :email_subscriptions, :message, :string
-    add_column :email_subscriptions, :phone, :string
-    add_column :email_subscriptions, :newsletter, :boolean
-  end
-end
