739e635e41d77497e21df098915611e7d5803daf
ajbraus <ajbraus@gmail.com>
2013-04-11 22:37:54 -0500

    blah

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  attr_accessible :img_url, :location, :name, :starts_at
+  attr_accessible :img_url, :location, :name, :starts_at, :subdomain
   has_many :attendees, through: :attendees
   has_one :organizer, through: :users
   validates :location, :name, :subdomain, :starts_at, :img_url, presence: true
